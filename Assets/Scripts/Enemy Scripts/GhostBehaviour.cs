6e623a2d63ebf005aa5f4f6cb6ecb6b43f82ee21
pariseve <136556166+pariseve@users.noreply.github.com>
2023-10-08 23:21:13 +1100

    ghost + level 4
    ghost behaviour script
    organised scripts folder
    level 4 (it sucks)

---
+++ Assets/Scripts/Enemy Scripts/GhostBehaviour.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class GhostBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed = 1f;            // slower movement speed
+    public float nextWaypointDistance = 3f;
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);  // Updated path less frequently
+    }
+
+    private void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+
+        // adds randomness to the direction for less accuracy
+        direction += Random.insideUnitCircle * 0.2f;
+
+        Vector2 force = direction * moveSpeed * Time.deltaTime;
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
