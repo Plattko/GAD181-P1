7015d4dedfd4aba1279b355c49083e661096c410
pariseve <136556166+pariseve@users.noreply.github.com>
2023-10-09 22:34:30 +1100

    level 5 added
    attempted cobweb obstacle - didn't work
    attempted screen shake effect - didn't work
    added level 5 (it sucks and i'm stressed out)

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeIntensity = 1f;
+    private float shakeTime = 0.2f;
+
+    private float timer;
+    private CinemachineBasicMultiChannelPerlin cbmcp;
+
+    private void Awake()
+    {
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        cbmcp = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        StopShake();
+    }
+
+    public void ShakeCamera()
+    {
+        cbmcp.m_AmplitudeGain = shakeIntensity;
+        timer = shakeTime;
+    }
+
+    void StopShake()
+    {
+        cbmcp.m_AmplitudeGain = 0f;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ShakeCamera();
+        }
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                StopShake();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ShakeCamera();
+        }
+    }
+}
---
+++ Assets/Scripts/Level Elements Scripts/Cobweb.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cobweb : MonoBehaviour
+{
+    private GameObject cobweb;
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Debug.Log("Player was caught in a cobweb");
+    //        Tutorial_GrapplingRope tutorial_GrapplingRope = other.GetComponent<Tutorial_GrapplingRope>();
+    //        if (tutorial_GrapplingRope != null)
+    //        {
+    //            //slow down player when entering cobweb
+    //            tutorial_GrapplingRope.SetSpeed(2f);
+    //        }
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Tutorial_GrapplingRope tutorial_GrapplingRope = other.GetComponent<Tutorial_GrapplingRope>();
+    //        if (tutorial_GrapplingRope != null)
+    //        {
+    //            tutorial_GrapplingRope.ResetSpeed();
+    //        }
+    //    }
+    //}
+}
